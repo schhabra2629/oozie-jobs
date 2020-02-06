c73f4d98deda4faaff6e409e1292db399ec2fa32
Shubham Chhabra <shubham@HW15728.local>
2020-02-06 10:00:39 +0530

    Added hive action

---
+++ oozie-samples/src/main/resources/scripts/hive-script.q
@@ -0,0 +1,1 @@
+select * from ${INPUT_TABLE}
--- oozie-samples/src/main/shell/generateOozieJobConfigs.sh
+++ oozie-samples/src/main/shell/generateOozieJobConfigs.sh
@@ -3,7 +3,8 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -u <ambari-url/CM> -w <workflow type> -o <otherComponent> -k <y|n> -f <frenquency> -s <startdate> -e <enddate>"
+   echo "Usage: $0 -c <clustername> -u <ambari-url/CM> -w <workflow type> -o <otherComponent> -k <y|n> -f <frenquency> -s <startdate> -e <enddate>"
+   echo -e "\t-c Cluster name"
    echo -e "\t-a ambari-url"
    echo -e "\t-w workflow type"
    echo -e "\t-o other component involved"
@@ -14,7 +15,7 @@ helpFunction()
    exit 1 # Exit script after printing help
 }
 
-while getopts "u:w:o:s:" opt
+while getopts "u:w:o:s:k:c:" opt
 do
    case "$opt" in
       u ) AMBARI_URL="$OPTARG" ;;
@@ -24,6 +25,7 @@ do
       f ) FREQ="$OPTARG" ;;
       s ) START_DATE="$OPTARG" ;;
       e ) END_DATE="$OPTARG" ;;
+      c ) CLUSTER_NAME="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
@@ -40,23 +42,43 @@ echo "$AMBARI_URL"
 
 # GET NAMENODE and RESOURCE MANAGER VALUES
 
-NAMENODE_HOST=$(curl -u admin:shubham -s http://172.25.42.144:8080/api/v1/clusters/c1293/services/HDFS/components/NAMENODE/ |grep host_name |awk -F ' : ' '{print $2}' |sed 's/"//g')
+
+NAMENODE_HOST=$(curl -u admin:shubham -s ${AMBARI_URL}/api/v1/clusters/${CLUSTER_NAME}/services/HDFS/components/NAMENODE/ |grep host_name |awk -F ' : ' '{print $2}' |sed 's/"//g')
 NAMENODE_URL="hdfs://${NAMENODE_HOST}:8020"
 
-RESOURCEMANAGER_HOST=$(curl -u admin:shubham -s http://172.25.42.144:8080/api/v1/clusters/c1293/services/YARN/components/RESOURCEMANAGER/ |grep host_name |awk -F ' : ' '{print $2}' |sed 's/"//g')
+RESOURCEMANAGER_HOST=$(curl -u admin:shubham -s ${AMBARI_URL}/api/v1/clusters/${CLUSTER_NAME}/services/YARN/components/RESOURCEMANAGER/ |grep host_name |awk -F ' : ' '{print $2}' |sed 's/"//g')
 RESOURCEMANAGER_URL="${RESOURCEMANAGER_HOST}:8050"
 
 
+mkdir $WF_TYPE
+wget https://raw.githubusercontent.com/schhabra2629/oozie-jobs/master/oozie-samples/src/main/workflows/$WF_TYPE-wf.xml -P $WF_TYPE
+hadoop fs -mkdir /tmp/$WF_TYPE
+hadoop fs -put $WF_TYPE/$WF_TYPE-wf.xml  /tmp/$WF_TYPE
+
   case $WF_TYPE in
 	shell)
+	    wget https://raw.githubusercontent.com/schhabra2629/oozie-jobs/master/oozie-samples/src/main/resources/scripts/sample-script.sh -P $WF_TYPE
+	    hadoop fs -put $WF_TYPE/sample-script.sh /tmp/$WF_TYPE
 
-	    mkdir $WF_TYPE
-	    wget <> -P $WF_TYPE
-	    wget <> -P $WF_TYPE
+	    echo "jobTracker=${RESOURCEMANAGER_URL}\
+nameNode=${NAMENODE_URL}
+shellScript=sample-script.sh
+shellScriptLoc=\${nameNode}/tmp/$WF_TYPE/sample-script.sh
+oozie.wf.application.path=\${nameNode}/tmp/$WF_TYPE/shell-wf.xml" > shell-job.properties
 
 		echo "Hello yourself!"
 		;;
-	bye)
+	hive)
+
+	 wget https://raw.githubusercontent.com/schhabra2629/oozie-jobs/master/oozie-samples/src/main/resources/scripts/hive-script.q -P $WF_TYPE
+	 hadoop fs -put $WF_TYPE/hive-script.q /tmp/$WF_TYPE
+
+	   echo "jobTracker=${RESOURCEMANAGER_URL}\
+nameNode=${NAMENODE_URL}
+script=hive-script.q
+scriptLoc=\${nameNode}/tmp/$WF_TYPE/hive-script.q
+oozie.wf.application.path=\${nameNode}/tmp/$WF_TYPE/hive-wf.xml" > hive-job.properties
+
 		echo "See you again!"
 		break
 		;;
